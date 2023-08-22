17bd34582d092d800bd2fac817fb0e3264848e55
janak <niraulajanak2019@gmail.com>
2023-08-22 14:49:34 +0545

    config: include theme overlay

--- config/common.mk
+++ config/common.mk
@@ -154,6 +154,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/awaken/prebuilt/common/etc/init/init.awaken-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.awaken-updater.rc
 
+#Themes OVerlay
+include vendor/Themes/themes.mk
+
 # Themes
 PRODUCT_PACKAGES += \
     AwakenBlackTheme \
